c8fddd8e6381efde18e5213b1913c417f01b0bc6
angel gustavo <en_ge_l@hotmail.com>
2023-10-05 22:23:22 -0600

    se agrego el codigo funcionando y comentando cada parte de ella

--- app.js
+++ app.js
@@ -8,10 +8,13 @@
 - Output - `['insecto', 'bootcamp', 'escritorio']`
  */
 
-const btn = document.querySelector("#btn");
+const btn = document.querySelector("#btn"); //obtenemos el boton para despues darle un evento click y ejecutar el comando
 const listWord = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
 
+
+//funcion que devuelve las palabas con mayor longitud que las de entrada
 function bigWords(word, list) {
+    //se usa el metodo filter para generar una nueva cadena   sin las palabras con menor longitud
     const newList = list.filter(item => {
         console.log(`word ${word}, item : ${item} `);
         return word.length < item.length;
@@ -27,7 +30,8 @@ function bigWords(word, list) {
 
 
 function imprimirLista(strings) {
-    const listElement = document.querySelector("#lista");
+    const listElement = document.querySelector("#lista");//obtenemos el elemento para agregar los li
+    //iteramos el array de palabras y creamos el elemento li por cada una de ellas agregandole el texto y agregandolo al elemento ul 
     strings.forEach(function (string) {
         const li = document.createElement('li');
         li.textContent = string;
@@ -35,11 +39,11 @@ function imprimirLista(strings) {
     });
 }
 
-// Ejemplo de uso
-console.log(btn);
+//usamos el elemento boton para que escuche un evento click  y agregemos el contenido en el html por medio del dom
 btn.addEventListener("click", evet => {
+    //obtenemos el valor de input para usar en la funcion big
     const palabra = document.querySelector("#palabras").value;
-    const newList = bigWords(palabra, listWord);
-    imprimirLista(newList);
+    const newList = bigWords(palabra, listWord);//obtenemos la nuevas palabras ya filtradas
+    imprimirLista(newList);// agregamos las palabras al html por medio del dom
 })
 
